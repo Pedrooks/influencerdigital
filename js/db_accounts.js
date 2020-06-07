116d7d23c73244c49582c3c627c93068af49caf6
Pedrooks <41552882+Pedrooks@users.noreply.github.com>
2020-06-07 11:18:36 -0300

    Add files via upload

---
+++ js/db_accounts.js
@@ -0,0 +1,11 @@
+﻿<!-- Begin
+function Login(){
+var done=0;
+var username=document.login.username.value;
+username=username.toLowerCase();
+var password=document.login.password.value;
+password=password.toLowerCase();
+if (username=="convidado" && password=="convidado") { window.location="http://zinaminados.blogspot.com.br/"; done=1; }
+if (done==0) { alert("Senha ou Usuário inválido."); }
+}
+// End -->
